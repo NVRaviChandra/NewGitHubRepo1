0142ba03aba3546b4e06fb25b48a589f98c6e18b
NVRaviChandra <nvravi.chandra@gmail.com>
2019-08-31 12:09:10 +0100

    This is second comment and version 2

--- GitProject/src/test/java/SampleGit.java
+++ GitProject/src/test/java/SampleGit.java
@@ -3,7 +3,8 @@ public class SampleGit {
 
 	public static void main(String[] args) {
 	System.out.println("We are in Git");
-//Thhis is my first comment
+//This is my first comment
+	//This is my second comment
 	}
 
 }
